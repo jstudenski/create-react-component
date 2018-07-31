ca79458e612ef7077e66881ee8a7d9f8df401525
jstudenski <jstudenski@gmail.com>
2018-07-30 20:28:46 -0500

    initial files read and output correctly

--- start.js
+++ start.js
@@ -1,5 +1,10 @@
 var writeFile = require('write');
 var inquirer = require('inquirer');
+const fs = require('fs');
+
+let sass = fs.readFileSync('template/template.sass', 'utf8');
+let main = fs.readFileSync('template/Template.js', 'utf8');
+
 
 inquirer.prompt([
   {
@@ -8,13 +13,34 @@ inquirer.prompt([
     name: "name",
   }
 ]).then(function(inquirerResponse) {
-  const name = inquirerResponse.name;
-  console.log(name);
-  // myGame.guess(guess) ? endGameMenu() : guessLetter();
+
+  let low = inquirerResponse.name.toLowerCase();
+  var up = low.charAt(0).toUpperCase() + low.substr(1)
+
+  console.log(low);
+  console.log(up);
+
+  // fs.readFile('template/hello.sass', function (err, data) {
+  //   if (err) throw err;
+  //   console.log(data);
+  // });
+
+  writeFile('output/'+up+'/'+up+'.js', main, function(err) {
+    if (err) console.log(err);
+  });
+
+  writeFile('output/'+up+'/'+low+'.sass', sass, function(err) {
+    if (err) console.log(err);
+  });
+
+  // writeFile('test/' + name +'.sass', index, function(err) {
+  //   if (err) console.log(err);
+  // });
+
 
 });
 
+// let index = "/n mport Sidebar from \'./Sidebar\'\; /n export default Sidebar\;"
 
-writeFile('test/foo', 'This is content...', function(err) {
-  if (err) console.log(err);
-});
+
+
