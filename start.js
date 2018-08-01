8fa7bc1210176dcaccbfa449bb5a6a249c2115c3
Jeff Studenski <jstudenski@gmail.com>
2018-08-01 09:19:54 -0500

    undo

--- start.js
+++ start.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 let sass = fs.readFileSync('template/template.scss', 'utf8');
 let main = fs.readFileSync('template/Template.js', 'utf8');
 
-
-
 inquirer.prompt([
   {
     type: "text",
