2cd89b921fc64b23c926bbc89f26ce994c2e1f03
Jeff Studenski <jstudenski@gmail.com>
2018-08-02 18:16:23 -0500

    begin error capture

--- start.js
+++ start.js
@@ -16,12 +16,24 @@ inquirer.prompt([
     message: "Stylesheet?",
     name: "stylesheet",
     choices: ['sass', 'css', 'none',]
+  },
+  {
+    type: 'list',
+    message: "Add Jest .test.js file?",
+    name: "tests",
+    choices: ['yes', 'no',]
   }
-]).then(function(inquirerResponse) {
+]).then(function(res) {
 
-  let low = inquirerResponse.name.toLowerCase();
+  if (res.name.length == 0) {
+    console.log("oops");
+  }
+
+  const styleSheet = res.stylesheet;
+  var low = res.name.toLowerCase();
   var up = low.charAt(0).toUpperCase() + low.substr(1)
 
+  console.log(styleSheet);
   console.log(low);
   console.log(up);
 
