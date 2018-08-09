c80d74081ccd0f6f318d87799b9b979e9fa5e148
Jeff Studenski <jstudenski@gmail.com>
2018-08-09 15:59:53 -0500

    add fuctions to generate file contents

--- start.js
+++ start.js
@@ -16,35 +16,37 @@ inquirer.prompt([{
     name: "stylesheet",
     choices: ['sass', 'css', 'none', ]
   },
-  {
-    type: 'list',
-    message: "Add Jest .test.js file?",
-    name: "tests",
+  // {
+  //   type: 'list',
+  //   message: "Add Jest .test.js file?",
+  //   name: "tests",
 
-    choices: ['yes', 'no',]
-  },
-  {
-    type: 'list',
-    message: "Has state?",
-    name: "state",
-    choices: ['yes', 'no',]
-  }
-]).then(function(res) {
-
-  if (res.name.length == 0) {
-    console.log("oops");
-  }
+  //   choices: ['yes', 'no',]
+  // },
+  // {
+  //   type: 'list',
+  //   message: "Has state?",
+  //   name: "state",
+  //   choices: ['yes', 'no',]
+  // }
+]).then(function (res) {
+// Error Handling:
+  // no component name
+  if (res.name.length == 0) {console.log("oops");}
 
   let name = res.name.toLowerCase()
-  let component;
-  // let low = res.name.toLowerCase();
-  component = {
+
+  let component = {
     nameLowercase: name,
     nameUppercase: name.charAt(0).toUpperCase() + name.substr(1),
+    styleSheet: res.stylesheet,
   }
 
-  console.log(component);
 
+  console.log(component);
+  console.log(props());
+
+  imports(component)
 
   // const styleSheet = res.stylesheet;
   // var low =
@@ -89,3 +91,25 @@ inquirer.prompt([{
   // writeStream.end();
 
 });
+
+imports = (component) => {
+  console.log("import React, { Component } from 'react';");
+  // External Stylesheet
+  component.styleSheet == 'css' || component.styleSheet == 'sass' ?
+    console.log("import './" + component.nameUppercase + "." + component.styleSheet + "';"):
+    null;
+}
+
+
+props = (name) => {
+  return getRandomColor();
+}
+
+getRandomColor = () => {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
